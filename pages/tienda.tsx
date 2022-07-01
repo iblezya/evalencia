d71944f6deb174247f68518591c038dd02ab89e7
Gerson Jean Pierre <iblezya@gmail.com>
2022-06-30 19:43:21 -0500

    add features to cart fix

--- pages/tienda.tsx
+++ pages/tienda.tsx
@@ -4,7 +4,7 @@ import { GetStaticProps } from 'next';
 
 import StoreComponentIndex from '../components/store';
 import { Product } from '../components/store/product/types';
-import { api } from '../components/store/product/api';
+import api from '../components/store/product/api';
 
 interface Props {
   products: Product[];
